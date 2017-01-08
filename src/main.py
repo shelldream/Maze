7cfb22afb8bd589fa94357f6259989e262376696
shelldream2048 <huangxiaojunfreeman@gmail.com>
2017-01-08 22:40:58 +0800

    replace tab to space

--- src/main.py
+++ src/main.py
@@ -16,34 +16,34 @@ def main():
 
     #model type
     parser.add_argument("--model", choices=["xgboost", ], dest="model", default="xgboost", help="Choose the model you want to use.\
-    	The choices include xgboost.")
+        The choices include xgboost.")
 
     #task type
     parser.add_argument("--task", choices=["regression", "classification", "ranking"], dest="task", default="regression",
-    	help="Choose the task type you want to use! The choices include regression, classification, ranking")
+        help="Choose the task type you want to use! The choices include regression, classification, ranking")
 
     #task mode
     parser.add_argument("--mode", choices=["train", "predict", "analysis"], dest="mode", default="train")
 
     #data path
     parser.add_argument("--train_data_path", action="append", dest="train_data", default=[], help="Choose the \
-    	training data path. You can choose more than one file path.")
+        training data path. You can choose more than one file path.")
     parser.add_argument("--test_data_path", action="append", dest="test_data", default=[], help="Choose the \
-    	test data path. You can choose more than one file path.")
+        test data path. You can choose more than one file path.")
 
     #parameter
     parser.add_argument("--parameters", default="{}", dest="parameters", help="Choose the parameters for your model and \
-    	the format of your parameters is a dict!")
+        the format of your parameters is a dict!")
 
     args = parser.parse_args()
     try:
-    	param_dict = eval(args.parameters)
+        param_dict = eval(args.parameters)
     except:
-    	raise ValueError("Wrong parameters!!")
+        raise ValueError("Wrong parameters!!")
     
     if args.model == "xgboost":
-    	if args.task == "classification":
-    		Xgb = Xgboost.Xgboost.XgboostClassifier()
+        if args.task == "classification":
+            Xgb = Xgboost.Xgboost.XgboostClassifier()
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -11,48 +11,48 @@ import os
 import pandas as pd
 
 def load_txt_file(fmap_filename="fmap.schema", file_list):
-	"""
-		载入文本数据
-		
-		Args:
-			file_list: list, 数据文件列表,数据格式按照 "\t" 分隔
-			fmap_filename: str, feature schema 文件, 格式：index\tfeature_name\tdata_type
-		Rets:
-			data: pandas dataframe 格式, 从多个数据文件中读取数据合并后的结果
-	"""
+    """
+        载入文本数据
+        
+        Args:
+            file_list: list, 数据文件列表,数据格式按照 "\t" 分隔
+            fmap_filename: str, feature schema 文件, 格式：index\tfeature_name\tdata_type
+        Rets:
+            data: pandas dataframe 格式, 从多个数据文件中读取数据合并后的结果
+    """
 
-	if not os.path.isfile(fmap_filename):
-		raise ValueError("%s does not exist!"%fmap_filename)
+    if not os.path.isfile(fmap_filename):
+        raise ValueError("%s does not exist!"%fmap_filename)
 
-	ft_name_type_dict = {}
-	ft_name_list = []
-	with open(fmap_filename, "r") as f_map_r:
-		for line in f_map_r:
-			try:
-				index, feature_name, data_type = line.rstrip("\t")
-				ft_name_list.append(feature_name)
-				ft_name_type_dict[feature_name] = data_type
-			except:
-				pass
+    ft_name_type_dict = {}
+    ft_name_list = []
+    with open(fmap_filename, "r") as f_map_r:
+        for line in f_map_r:
+            try:
+                index, feature_name, data_type = line.rstrip("\t")
+                ft_name_list.append(feature_name)
+                ft_name_type_dict[feature_name] = data_type
+            except:
+                pass
 
-	frames = []
-	for filename in file_list:
-		if not os.path.isfile(filename):
-			raise ValueError("%s does not exist!"%filename)
-		df = pd.read_csv(filename, sep="\t", header=None, names=ft_name_list, \
-			dtype=ft_name_type_dict)
-		frames.append(df)
-	data = pd.concat(frames, axis=0)
-	data = data.reset_index(drop=True)  #合并不同数据文件的数据，然后重置index
+    frames = []
+    for filename in file_list:
+        if not os.path.isfile(filename):
+            raise ValueError("%s does not exist!"%filename)
+        df = pd.read_csv(filename, sep="\t", header=None, names=ft_name_list, \
+            dtype=ft_name_type_dict)
+        frames.append(df)
+    data = pd.concat(frames, axis=0)
+    data = data.reset_index(drop=True)  #合并不同数据文件的数据，然后重置index
 
-	return data
+    return data
 
 
 def filter_feature(filter_feature_list=None, data):
-	"""
-		Args:
-			filter_feature_list: list,
-			data: pandas dataframe 格式
-		Rets:
+    """
+        Args:
+            filter_feature_list: list,
+            data: pandas dataframe 格式
+        Rets:
 
-	"""
+    """
